76624a38c66aefa089b4627609d9e4d868e9fbbe
Saurav Shekhar <weblife.shekhar24@gmail.com>
2024-01-08 15:48:08 +0530

    NFT UUPS Upgradble Basic Tests

---
+++ contracts/Base.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+
+contract ChooseNFTPackage {
+
+    uint256 public constant NFT_PACKAGE_1 = 1;
+    uint256 public constant NFT_PACKAGE_2 = 2;
+    uint256 public constant NFT_PACKAGE_3 = 3;
+    uint256 public constant NFT_PACKAGE_4 = 4;
+    uint256 public constant NFT_PACKAGE_5 = 5;
+   
+ 
+
+
+}
